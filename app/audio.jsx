06f2f55cb1e17062a2804cb0e805015f99914bc5
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-04-20 08:19:24 -0400

    fix: build issue fix

--- app/audio.jsx
+++ app/audio.jsx
@@ -24,6 +24,16 @@ export default function AudioComponent() {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  const handlePlay = () => {
+    // Check if responsiveVoice is loaded
+    if (responsiveVoice) {
+      console.log("ResponsiveVoice loaded");
+      responsiveVoice.speak(transcript);
+    } else {
+      console.error("ResponsiveVoice not loaded");
+    }
+  };
+
   if (!browserSupportsSpeechRecognition) {
     console.log("Browser does not support Speech Recognition");
   }
@@ -151,10 +161,28 @@ export default function AudioComponent() {
                 />
               </svg>
             </button>
-          </div>
-          {/* Progress Bar */}
 
-          {/* Time Information */}
+            <button
+              className="p-3 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none"
+              onClick={handlePlay}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+                />
+              </svg>
+            </button>
+          </div>
+
           <div className="flex justify-between mt-2 text-sm text-gray-600">
             <div className="mx-auto max-w-7xl  sm:px-6 lg:px-8">
               <p>{transcript}</p>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
+"use client";
 import Image from "next/image";
 import AudioComponent from "./audio";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <main className="bg-white">
-      <AudioComponent />
-    </main>
+    <>
+      <Script
+        src="https://code.responsivevoice.org/responsivevoice.js?key=B4hVmSBj"
+        strategy="beforeInteractive"
+        onLoad={() =>
+          console.log(
+            `script loaded correctly, responsivevoice  been populated`
+          )
+        }
+      />
+      <main className="bg-white">
+        <AudioComponent />
+      </main>
+    </>
   );
 }
